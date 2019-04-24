6f9b8b9a851ca15d744c66b18a791a73af2ff7bf
Jon Arme <jarme@grubhub.com>
2019-04-23 23:28:05 -0500

    latest.

---
+++ src/bg-image.js
@@ -0,0 +1,17 @@
+import React from 'react'
+import styled from 'styled-components';
+
+const Img = styled.img`
+  width: 100vw;
+  position: absolute;
+  left: 0;
+  z-index: -1;
+  top: 50%;
+  transform: translateY(-50%);
+`;
+
+const BGImage = ({ src }) => {
+  console.log(src);
+  return <Img src={src} />;
+}
+export default BGImage;
---
+++ theme.js
@@ -0,0 +1,9 @@
+import theme from 'mdx-deck/themes';
+
+export default {
+  ...theme,
+  font: 'Futura, sans-serif',
+  colors: {
+    background: 'transparent'
+  }
+}
